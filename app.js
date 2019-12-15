795e475e39108cfcaf297e37557da808e74a9149
katetho <58860379+katetho@users.noreply.github.com>
2019-12-15 15:24:20 +0200

    connected to MongoDB
    created a schema for tickets in MongoDB. cors - for accessing the data collection from a remote host

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+const ticketsRoute = require('./routes/tickets');
+const cors = require('cors');
 
-app.get('/', (req, res)=> {
-  res.send('hello anton');
-})
+app.use(bodyParser.json());
+app.use(cors());
+//Middleware
+app.use('/tickets', ticketsRoute);
+
+
+app.get('/', (req, res) => {
+	res.send("Response sent");
+});
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useUnifiedTopology: true, useNewUrlParser: true },
+	() => {
+	console.log('connected to DB');
+});
 
 app.listen(3002);
---
+++ models/Ticket.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const TicketSchema = mongoose.Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+module.exports = mongoose.model('Tickets', TicketSchema);
