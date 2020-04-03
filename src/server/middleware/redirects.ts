78abb251c44af21cac109044c8e3643674f1e4a7
katetho <58860379+katetho@users.noreply.github.com>
2020-04-03 13:01:01 +0300

    redirects

--- src/server/middleware/redirects.ts
+++ src/server/middleware/redirects.ts
@@ -1,18 +1,13 @@
 import { Request, Response, NextFunction} from 'express';
 
-export const redirects = {
-    redirectSignin: function(req: Request, res: Response, next: NextFunction): void { //redirect unauthenticated users
-        if (req.session && !req.session.userId) {
-            res.redirect('/users/signin')
-        } else {
-            next();
-        }
-    },
-    redirectHome: function(req: Request, res: Response, next: NextFunction): void { //redirect authenticated users
-        if (req.session && req.session.userId) {
-            res.redirect('/')
-        } else {
-            next();
-        }
+export const redirects = function(req: Request, res: Response, next: NextFunction): void { //redirect unauthenticated users
+    let url = req.originalUrl;
+    if (req.session && !req.session.userId && !url.includes('users')) {
+        res.redirect('/users/signin')
+    } else if (req.session && req.session.userId && url.includes('user')) {
+        res.redirect('/')
+    }
+    else {
+        next();
     }
 }
--- src/server/routes/routes.ts
+++ src/server/routes/routes.ts
@@ -6,24 +6,25 @@ import * as ticketsRoutes from './ticketsController';
 import * as usersRoutes from './usersController';
 import { redirects } from '../middleware/redirects';
 
-router.get('/', redirects.redirectSignin, homeRoutes.list);
-router.get('/mytickets', redirects.redirectSignin, homeRoutes.mytickets);
-router.get('/taketicket', redirects.redirectSignin, homeRoutes.taketicket);
-router.get('/outgoing', redirects.redirectSignin, homeRoutes.outgoing);
+router.use(redirects);
+router.get('/', homeRoutes.list);
+router.get('/mytickets', homeRoutes.mytickets);
+router.get('/taketicket', homeRoutes.taketicket);
+router.get('/outgoing', homeRoutes.outgoing);
 
-router.get('/tickets', redirects.redirectSignin, ticketsRoutes.getAllTickets);
-router.post('/tickets', redirects.redirectSignin, ticketsRoutes.postTicket);
-router.get('/tickets/:ticketId', redirects.redirectSignin, ticketsRoutes.findTicket);
-router.delete('/tickets/:ticketId', redirects.redirectSignin, ticketsRoutes.deleteTicket);
-router.patch('/tickets/close', redirects.redirectSignin, ticketsRoutes.closeTicket);
-router.patch('/tickets/:ticketId', redirects.redirectSignin, ticketsRoutes.takeTicket);
+router.get('/tickets', ticketsRoutes.getAllTickets);
+router.post('/tickets', ticketsRoutes.postTicket);
+router.get('/tickets/:ticketId', ticketsRoutes.findTicket);
+router.delete('/tickets/:ticketId', ticketsRoutes.deleteTicket);
+router.patch('/tickets/close', ticketsRoutes.closeTicket);
+router.patch('/tickets/:ticketId', ticketsRoutes.takeTicket);
 
-router.get('/users/register', redirects.redirectHome, usersRoutes.getRegister);
-router.post('/users/register', redirects.redirectHome, usersRoutes.postRegister);
-router.get('/users/signin', redirects.redirectHome, usersRoutes.getSignin);
-router.post('/users/signin', redirects.redirectHome, usersRoutes.postSignin);
-router.post('/users/forgot-password', redirects.redirectHome, usersRoutes.forgotPassword);
-router.get('/users/signout', redirects.redirectSignin, usersRoutes.signout);
+router.get('/users/register', usersRoutes.getRegister);
+router.post('/users/register', usersRoutes.postRegister);
+router.get('/users/signin', usersRoutes.getSignin);
+router.post('/users/signin', usersRoutes.postSignin);
+router.post('/users/forgot-password', usersRoutes.forgotPassword);
+router.get('/users/signout', usersRoutes.signout);
 
 router.use((req: Request, res: Response) => { //page not found
     res.render('404', {
