393a7777ff70438b2091967517d28178582347e3
katetho <58860379+katetho@users.noreply.github.com>
2020-05-03 17:29:14 +0300

    heroku

--- public/server/config/database.js
+++ public/server/config/database.js
@@ -2,5 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 //Connect to DB
 const sequelize_typescript_1 = require("sequelize-typescript");
-exports.db = new sequelize_typescript_1.Sequelize('mysql://b815ccf5030b93:868175ea@eu-cdbr-west-03.cleardb.net/heroku_92b0692982f5285');
+exports.db = new sequelize_typescript_1.Sequelize('mysql://b815ccf5030b93:868175ea@eu-cdbr-west-03.cleardb.net/heroku_92b0692982f5285', {
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 1000000,
+        idle: 40000
+    }
+});
 //# sourceMappingURL=database.js.map
--- src/server/config/database.ts
+++ src/server/config/database.ts
@@ -1,3 +1,10 @@
 //Connect to DB
 import { Sequelize } from 'sequelize-typescript';
-export const db = new Sequelize('mysql://b815ccf5030b93:868175ea@eu-cdbr-west-03.cleardb.net/heroku_92b0692982f5285');
+export const db = new Sequelize('mysql://b815ccf5030b93:868175ea@eu-cdbr-west-03.cleardb.net/heroku_92b0692982f5285', {
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 1000000,
+        idle: 40000
+    }
+});
